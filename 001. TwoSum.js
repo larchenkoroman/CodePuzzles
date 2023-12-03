9554d84e6cad587000a89d1def03cd437909331f
larchenkoroman <larchenkoroman@gmail.com>
2023-12-03 14:29:57 +0200

    TwoSum

--- 001. TwoSum.js
+++ 001. TwoSum.js
@@ -9,20 +9,21 @@ Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 */
 function TwoSum(numbers, target){
-   let
-      mapping = {},
+   const
+      mapping = new Map(),
       result = []
+   let
       searched = 0;
 
    for(let i = 0; i < numbers.length; i++){
-      mapping[numbers[i]] = i;
+      mapping.set(numbers[i], i);
    }
-
    for(let i = 0; i < numbers.length; i++){
       searched = target - numbers[i];
-      if(mapping[searched]){
+
+      if(mapping.has(searched)){
          result.push(i + 1);
-         result.push(mapping[searched] + 1);
+         result.push(mapping.get(searched) + 1);
          break;
       }
    }
@@ -30,5 +31,5 @@ function TwoSum(numbers, target){
 }
 
 let input = [2, 7, 11, 15];
-console.log(TwoSum(input, 26));
-console.log(TwoSum(input, 22));
+console.log(TwoSum(input, 18));
+console.log(TwoSum(input, 13));
